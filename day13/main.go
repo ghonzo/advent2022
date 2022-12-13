cd9d3b8e32033ef84a6e9a5a361be4e40f95e82e
Eric Lunt <eric@lunt.software>
2022-12-13 01:09:19 -0600

    Day 13 cleanup

--- day13/main.go
+++ day13/main.go
@@ -22,7 +22,7 @@ func main() {
 func part1(entries []string) int {
 	sum := 0
 	for i := 1; i <= (len(entries)+1)/3; i++ {
-		if inOrderStr(entries[(i-1)*3], entries[(i-1)*3+1]) {
+		if inOrder(entries[(i-1)*3], entries[(i-1)*3+1]) {
 			sum += i
 		}
 	}
@@ -40,7 +40,7 @@ func part2(entries []string) int {
 	divPacket2 := toPacketData("[[6]]")
 	allPackets = append(allPackets, divPacket1, divPacket2)
 	sort.Slice(allPackets, func(i, j int) bool {
-		return inOrder(allPackets[i], allPackets[j]) < 0
+		return compare(allPackets[i], allPackets[j]) < 0
 	})
 	key := 1
 	for i, pd := range allPackets {
@@ -51,20 +51,25 @@ func part2(entries []string) int {
 	return key
 }
 
+// Kind of a sloppy data structure. If isVal is true, then use val. Otherwise, use list.
 type packetData struct {
 	isVal bool
 	val   int
 	list  []*packetData
 }
 
-func inOrderStr(left, right string) bool {
-	lpd := toPacketData(left)
-	rpd := toPacketData(right)
-	return inOrder(lpd, rpd) < 0
+// Create a packetData that represents a value
+func number(v int) *packetData {
+	return &packetData{isVal: true, val: v}
+}
+
+func inOrder(left, right string) bool {
+	return compare(toPacketData(left), toPacketData(right)) < 0
 }
 
 func toPacketData(s string) *packetData {
 	stack := lane.NewStack(new(packetData))
+	// Set to true if we're in the middle of parsing a number
 	var inNum bool
 	for _, r := range s {
 		if r == '[' {
@@ -74,13 +79,12 @@ func toPacketData(s string) *packetData {
 			v := int(r - '0')
 			h, _ := stack.Head()
 			if inNum {
-				// two digit number
+				// mulit-digit number
 				lastNumPd := h.list[len(h.list)-1]
 				lastNumPd.val *= 10
 				lastNumPd.val += v
 			} else {
-				numPd := &packetData{isVal: true, val: v}
-				h.list = append(h.list, numPd)
+				h.list = append(h.list, number(v))
 				inNum = true
 			}
 		} else if r == ',' {
@@ -97,27 +101,29 @@ func toPacketData(s string) *packetData {
 }
 
 // negative if in order, postive if not in order, 0 if equal
-func inOrder(left, right *packetData) int {
+func compare(left, right *packetData) int {
 	if left.isVal && right.isVal {
 		return left.val - right.val
 	}
+	leftList := left.list
 	if left.isVal {
-		left.list = []*packetData{{isVal: true, val: left.val}}
+		leftList = []*packetData{number(left.val)}
 	}
+	rightList := right.list
 	if right.isVal {
-		right.list = []*packetData{{isVal: true, val: right.val}}
+		rightList = []*packetData{number(right.val)}
 	}
-	for i, lv := range left.list {
-		if i >= len(right.list) {
+	for i, lv := range leftList {
+		if i >= len(rightList) {
 			// Right ran out first
 			return 1
 		}
-		if ord := inOrder(lv, right.list[i]); ord != 0 {
+		if ord := compare(lv, rightList[i]); ord != 0 {
 			return ord
 		}
 	}
 	// Did left run out?
-	if len(left.list) < len(right.list) {
+	if len(leftList) < len(rightList) {
 		// Yep
 		return -1
 	}
