a50d499d34382aa0e3255cb727e6608df81bd638
Eric Lunt <eric@lunt.software>
2022-12-21 00:01:06 -0600

    Day 21 part 1 test

---
+++ day21/main_test.go
@@ -0,0 +1,28 @@
+// Advent of Code 2022, Day 21
+package main
+
+import (
+	"testing"
+
+	"github.com/ghonzo/advent2022/common"
+)
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		entries []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 152},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.entries); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
