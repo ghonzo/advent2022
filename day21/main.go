da46b2cc7553aab49b16b1aa10491dc58f0d4cb6
Eric Lunt <eric@lunt.software>
2022-12-20 23:58:39 -0600

    Day 21 "solution"

--- day21/main.go
+++ day21/main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"unicode"
 
 	"github.com/ghonzo/advent2022/common"
@@ -15,12 +17,15 @@ func main() {
 	fmt.Println("Advent of Code 2022, Day 21")
 	entries := common.ReadStringsFromFile("input.txt")
 	fmt.Printf("Part 1: %d\n", part1(entries))
-	//fmt.Printf("Part 2: %d\n", part2(entries))
+	fmt.Printf("Part 2: %s\n", part2(entries))
 }
 
 type monkey struct {
-	name        string
-	value       int
+	name string
+	// use in part 1
+	value int
+	// use in part 2
+	valueExpr   string
 	hasValue    bool
 	left, right string
 	op          byte
@@ -32,6 +37,19 @@ func part1(entries []string) int {
 		monkeyMap[m.name] = m
 	}
 	return findValue(monkeyMap, "root")
+}
+
+func part2(entries []string) string {
+	monkeyMap := make(map[string]*monkey)
+	for _, m := range readMonkeys(entries) {
+		if m.hasValue {
+			m.valueExpr = strconv.Itoa(m.value)
+		}
+		monkeyMap[m.name] = m
+	}
+	monkeyMap["root"].op = '='
+	monkeyMap["humn"].valueExpr = "x"
+	return findValue2(monkeyMap, "root")
 }
 
 func readMonkeys(entries []string) []*monkey {
@@ -70,4 +88,47 @@ func findValue(monkeyMap map[string]*monkey, name string) int {
 	}
 	m.hasValue = true
 	return m.value
+}
+
+func findValue2(monkeyMap map[string]*monkey, name string) string {
+	m := monkeyMap[name]
+	if m.hasValue {
+		return m.valueExpr
+	}
+	lExpr := findValue2(monkeyMap, m.left)
+	lv, err1 := strconv.Atoi(lExpr)
+	rExpr := findValue2(monkeyMap, m.right)
+	rv, err2 := strconv.Atoi(rExpr)
+	if err1 == nil && err2 == nil {
+		switch m.op {
+		case '+':
+			m.valueExpr = strconv.Itoa(lv + rv)
+		case '-':
+			m.valueExpr = strconv.Itoa(lv - rv)
+		case '*':
+			m.valueExpr = strconv.Itoa(lv * rv)
+		case '/':
+			m.valueExpr = strconv.Itoa(lv / rv)
+		}
+	} else {
+		var sb strings.Builder
+		if err1 != nil {
+			sb.WriteByte('(')
+			sb.WriteString(lExpr)
+			sb.WriteByte(')')
+		} else {
+			sb.WriteString(lExpr)
+		}
+		sb.WriteByte(m.op)
+		if err2 != nil {
+			sb.WriteByte('(')
+			sb.WriteString(rExpr)
+			sb.WriteByte(')')
+		} else {
+			sb.WriteString(rExpr)
+		}
+		m.valueExpr = sb.String()
+	}
+	m.hasValue = true
+	return m.valueExpr
 }
