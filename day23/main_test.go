7c745c3bfa864632c3682f03e28f8070e122067f
Eric Lunt <eric@lunt.software>
2022-12-23 00:09:21 -0600

    Day 23 tests

---
+++ day23/main_test.go
@@ -0,0 +1,48 @@
+// Advent of Code 2022, Day 23
+package main
+
+import (
+	"testing"
+
+	"github.com/ghonzo/advent2022/common"
+)
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		entries []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 110},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.entries); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		entries []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 20},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.args.entries); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
